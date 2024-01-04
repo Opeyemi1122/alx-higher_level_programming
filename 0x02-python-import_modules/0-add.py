384f33f4525ca96f2a60a9ed54d9ae9a9533b97f
Opeyemi1122 <alabiopex@gmail.com>
2024-01-04 18:29:28 +0100

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print("{a} + {b} = {result}".format(a=a, b=b, result=result))
+
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
