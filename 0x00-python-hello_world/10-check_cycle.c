ab216007248d8b05d77df7d6558b54480a4c91f3
Opeyemi1122 <alabiopex@gmail.com>
2024-01-04 13:45:44 +0100

    check cycle c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * check_cycle - checks is a singly linked list has a cycle in it.
+ *
+ * Return 0 if no cycle
+ */
+
+int check_cycle(listint_t *list);
+{
+	return (0);
+}
