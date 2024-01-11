f15bb33164a26b51cbe811f16911494e13ac66d6
Opeyemi1122 <alabiopex@gmail.com>
2024-01-11 13:49:05 +0100

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    for index in range(len(my_list)):
+        if my_list[index] == search:
+            my_list[index] = replace
+
+
+if __name__ == "__main__":
+    print(new_list)
+    print(my_list)
