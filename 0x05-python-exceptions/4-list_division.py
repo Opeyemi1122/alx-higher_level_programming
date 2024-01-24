72f2b1ec4983139074aeea6956059599ee2c188c
Opeyemi1122 <alabiopex@gmail.com>
2024-01-24 12:19:28 +0100

    safe print list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,9 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             element_1 = my_list_1[i] if i < len(my_list_1) else 0
-            
             element_2 = my_list_2[i] if i < len(my_list_2) else 0
-
             division_result = element_1 / element_2
             result.append(division_result)
         except ZeroDivisionError:
