28efb140cf0c37a4c3894fb794d2f760e761013d
Opeyemi1122 <alabiopex@gmail.com>
2024-02-05 21:14:28 +0100

    inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-inherits_from: function to run obj and a_class"""
+"""inherits_from: function to run obj and a_class"""
 
 
 def inherits_from(obj, a_class):
-    """
-    returns true or false if instance of an object is inherited"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """returns true or false if instance of an object is inherited"""
+    #return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
