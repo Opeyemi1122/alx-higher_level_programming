eeb618aac239514960587c53180e0c1d5c9c4e84
Opeyemi1122 <alabiopex@gmail.com>
2024-02-06 23:06:13 +0100

    base geometry exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class BaseGeometry: based on previous file"""
+
+
+class BaseGeometry:
+    def area(self):
+        """public instance method used and exception implemented with text"""
+        raise Exception("area() is not implemented")
