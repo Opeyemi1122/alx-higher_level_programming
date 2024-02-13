b1e0dec6ceb36d060801f2b97fe64d6b3efd308b
Opeyemi1122 <alabiopex@gmail.com>
2024-02-13 15:51:27 +0100

    achieved function, area of the rectangle, height times width

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """area self is height times width"""
+        return self.height * self.width
